2f9e59d843e4f4de782925a924a0c7cdfdf91c78
Oussama <oussama.elbouhtani@student.vinci.com>
2023-01-20 11:02:11 +0100

    app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 /* istanbul ignore file */
 try {
     const { isEmpty } = require("./services/validations");
+    const { isValide}=  require("./services/validations");
     const gamerTagInput = document.getElementById("gamerTagInput");
     const checkButton = document.getElementById("gamerTagCheckButton");
     const feedbackMessage = document.getElementById("gamerTagFeedback");
@@ -15,8 +16,9 @@ try {
     checkButton.addEventListener("click", () => {
         // TODO: Add the logic to display the correct feedback message (error and validation)
         feedbackMessageText = isEmpty(gamerTagValue)
-            ? "Gamer tag cannot be empty"
-            : "Gamer tag is valid";
+            if(feedbackMessageText== true ) return "Gamer tag cannot be empty"
+        feedbackMessageText = isValide(gamerTagValue)
+            if(feedbackMessageText): "Gamer tag is valid";
         feedbackMessage.textContent = feedbackMessageText;
     });
 } catch (err) {
